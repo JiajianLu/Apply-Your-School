3ae91457206e4bce3087204f532d2b4aab465a0d
Jiajian Lu <jiajianlu@Jiajians-MacBook-Pro.local>
2018-04-13 00:56:38 -0700

    Bugs in import function. Users can type the school name or just type rank and states to search school.

--- modified.py
+++ modified.py
@@ -6,7 +6,7 @@ import json
 # Connect to the database
 connection = pymysql.connect(host='localhost',
                              user='root',
-                             password='123Ace1994@',
+                             password='123ace1994',
                              db='new_database',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
@@ -17,17 +17,37 @@ app = Flask(__name__,static_url_path="/static")
 def get_school():
     with connection.cursor() as cursor:
     # Read a single record
+    	#get school_name from the passed parameters
         school_name = request.args.get('school_name')
-        sql = "SELECT * FROM schools where school=%s"
-        cursor.execute(sql, (school_name,))
+        if school_name:
+            sql = "SELECT * FROM new_schools where school=%s"
+            cursor.execute(sql, (school_name,))
+            
+        else:
+            rank1 = request.args.get('rank1')
+            rank2 = request.args.get('rank2')
+            states = request.args.getlist('states')
+            format_strings = ','.join(['%s'] * len(states))
+            sql = "SELECT * FROM new_schools where rank>=%s and rank <=%s and state in (%s)"
+            sql = sql % ('%s','%s',format_strings) 
+            value = (int(rank1), int(rank2))
+            for state in states:
+                value += (state,)
+            print(value)
+            cursor.execute(sql, value)
+            #not enough arguement for format_strings
         results = cursor.fetchall()
         schools = list()
-
         for i in range(len(results)):
             school = results[i]
             schools.append(school)
         #print(schools)
         return json.dumps(schools)
 
-# def file_upload():
-#     with connection.cursor() as cursor:
+@app.route('/import', methods = ['POST'])
+def file_upload():
+    with connection.cursor() as cursor:
+    	#get the list of files
+    	print('a')
+    	uploaded_files = request.files.getlist("file[]")
+    	print(uploaded_files)
--- webserver.py
+++ webserver.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, session
 import requests
 import pymysql
 
-app = Flask(__name__,static_url_path="/statice")
+app = Flask(__name__,static_url_path="/static")
 
 api_address = 'https://0.0.0.0:5001/'
 
@@ -15,15 +15,29 @@ def get_schools(school_name = "ALL"):
 	schools = get_schools.json()
 	return schools
 
+def get_schools_by_rank_state(rank1=1, rank2=5, states = ['California']):
+	params = {'rank1': rank1, 'rank2': rank2, 'states': states}
+	get_schools = requests.get("http://localhost:5001/get_schools", params = params)
+	#print(get_schools)
+	schools = get_schools.json()
+	return schools
+
 @app.route('/', methods = ['GET'])
 def get_homepage():
 	return render_template("Users.html")
 
 @app.route('/', methods = ['POST'])
 def post_homepage():
-	name = request.form.get('school_name')
-	schools = get_schools(name)
-	print(schools)
+	name = request.form.get('school_name') #get form element according to name
+	if name: #if name is not empty
+		schools = get_schools(name)
+	else:
+		rank1 = request.form.get('ranking_range1')
+		rank2 = request.form.get('ranking_range2')
+		states = request.form.getlist('states')
+		#print(states)
+		schools = get_schools_by_rank_state(rank1, rank2, states)
+	#print(schools)
 	return render_template("Users.html", contents = schools)
 
 @app.route('/import', methods = ['GET'])
